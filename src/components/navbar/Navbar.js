21d5dca557408c6a51192ccdd727d8f2c0be81b5
Frenzoid <frenzoid@protonmail.com>
2021-08-18 18:09:35 +0200

    updated footer position

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,9 +7,9 @@ function Navbar(props) {
         <header className="displayed-content py-3">
             <div className="d-flex flex-row justify-content-around flex-wrap">
                 <div className="d-flex flex-row justify-content-center flex-wrap">
-                    <div className="navbar-button mx-3" style={props.location === "changed" ? { color: "white" } : {}} onClick={() => props.setLocation('changed')}>{props.locale.navbar.changed}</div>
-                    <div className="navbar-button mx-3" style={props.location === "changedSpecial" ? { color: "white" } : {}} onClick={() => props.setLocation('changedSpecial')}>{props.locale.navbar.changedSpecial}</div>
-                    <div className="navbar-button mx-3" style={props.location === "about" ? { color: "white" } : {}} onClick={() => props.setLocation('about')}>{props.locale.navbar.about}</div>
+                    <div className="navbar-button mx-3 my-auto" style={props.location === "changed" ? { color: "white" } : {}} onClick={() => props.setLocation('changed')}>{props.locale.navbar.changed}</div>
+                    <div className="navbar-button mx-3 my-auto" style={props.location === "changedSpecial" ? { color: "white" } : {}} onClick={() => props.setLocation('changedSpecial')}>{props.locale.navbar.changedSpecial}</div>
+                    <div className="navbar-button mx-3 my-auto" style={props.location === "about" ? { color: "white" } : {}} onClick={() => props.setLocation('about')}>{props.locale.navbar.about}</div>
                 </div>
                 <div className="d-flex flex-row justify-content-center flex-wrap">
                     <div className="navbar-text mx-2">{props.locale.navbar.language}</div>
