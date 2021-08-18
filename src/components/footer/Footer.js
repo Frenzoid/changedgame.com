3ac08bd0e0620921df99edf6c32fc3eea43961ad
Frenzoid <frenzoid@protonmail.com>
2021-08-18 12:01:08 +0200

    updated footer, made it properly responsive

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,22 +5,26 @@ function Footer(props) {
     return (
         <footer className="footer pt-2 pb-4 mb-0 displayed-content">
             <div className="container">
-                <div className="d-flex flex-row flex-wrap p-0 mb-0 justify-content-around">
-                    <a href={"https://twitter.com/DragonSnow4"} className="footer-item">
-                        <img alt="footer-badge" src={"https://img.shields.io/badge/-SnowDragon-3675ea?style=flat-square&logo=twitter&logoColor=white"} />
-                    </a>
-                    <a href={"https://www.patreon.com/DragonSnow"} className="footer-item">
-                        <img alt="footer-badge" src={"https://img.shields.io/badge/-SnowDragon-A8131D?style=flat-square&logo=patreon&logoColor=white"} />
-                    </a>
+                <div className="d-flex flex-row flex-nowrap p-0 mb-0 justify-content-around">
+                    <div className="d-flex flex-row flex-wrap justify-content-center">
+                        <a href={"https://twitter.com/DragonSnow4"} className="footer-item">
+                            <img alt="footer-badge" src={"https://img.shields.io/badge/-SnowDragon-3675ea?style=flat-square&logo=twitter&logoColor=white"} />
+                        </a>
+                        <a href={"https://www.patreon.com/DragonSnow"} className="footer-item">
+                            <img alt="footer-badge" src={"https://img.shields.io/badge/-SnowDragon-A8131D?style=flat-square&logo=patreon&logoColor=white"} />
+                        </a>
+                    </div>
                     <p className="text-center">
                         {locale.footer.allrights}
                     </p>
-                    <a href={"https://space.bilibili.com/753832"} className="footer-item">
-                        <img alt="footer-badge" src={"https://img.shields.io/badge/-雪之龙-FFFFFF?style=flat-square&logo=bilibili&logoColor=blue"} />
-                    </a>
-                    <a href={"https://steamcommunity.com/profiles/76561198181374861"} className="footer-item">
-                        <img alt="footer-badge" src={"https://img.shields.io/badge/-SnowDragon-1b2838?style=flat-square&logo=steam&logoColor=white"} />
-                    </a>
+                    <div className="d-flex flex-row flex-wrap justify-content-center" >
+                        <a href={"https://space.bilibili.com/753832"} className="footer-item">
+                            <img alt="footer-badge" src={"https://img.shields.io/badge/-雪之龙-FFFFFF?style=flat-square&logo=bilibili&logoColor=blue"} />
+                        </a>
+                        <a href={"https://steamcommunity.com/profiles/76561198181374861"} className="footer-item">
+                            <img alt="footer-badge" src={"https://img.shields.io/badge/-SnowDragon-1b2838?style=flat-square&logo=steam&logoColor=white"} />
+                        </a>
+                    </div>
                 </div>
             </div>
         </footer>
