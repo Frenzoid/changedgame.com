07c8f670a9f1b79d58e032fb276ad7f55ad40a02
Frenzoid <frenzoid@protonmail.com>
2021-08-18 04:36:10 +0200

    fixed responsive issues in about page

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -4,11 +4,11 @@ function About(props) {
 
     return (
         <main className="container pb-3 displayed-content" id="about-main">
-            <div className="d-flex flex-row flex-wrap">
+            <div className="d-flex flex-row flex-wrap justify-content-center">
                 <div id="dragonSnow-logo">
                     <img className="img-fluid d-block ms-4" src={dragonSnowAvatar} alt="DragonSnow Avatar logo" />
                 </div>
-                <div className="lead d-flex flex-column flex-wrap m-auto social-container">
+                <div className="lead d-flex flex-column flex-wrap m-auto px-2 pt-4 social-container">
                     <p className="text-warning">
                         {props.locale.about.name}
                     </p>
@@ -16,15 +16,15 @@ function About(props) {
                         patreon.com/DragonSnow
                     </a>
                     <a href="https://steamcommunity.com/profiles/76561198181374861">
-                        steamcommunity.com/profiles/SnowDragon
+                        steamcommunity.com/SnowDragon
                     </a>
                 </div>
-                <div className="lead d-flex flex-column flex-wrap m-auto social-container">
+                <div className="lead d-flex flex-column flex-wrap m-auto px-2 pt-4 social-container">
                     <a href="https://twitter.com/DragonSnow4">
                         twitter.com/DragonSnow4
                     </a>
                     <a href="https://www.furaffinity.net/user/drdragontim/">
-                        furaffinity.net/user/drdragontim/
+                        furaffinity.net/drdragontim/
                     </a>
                     <a href="https://space.bilibili.com/753832">
                         bilibili.com/雪之龙
